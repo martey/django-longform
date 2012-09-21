134bcc0a0f1fefb7afbcef1e54dfa7c1581f9193
Martey Dodoo <martey@marteydodoo.com>
2012-09-21 06:17:10 -0400

    Make sure `longform` dir is properly installed.
    I am officially *the worst* at packaging.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 
 setup(
     name='django-longform',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django application for longform blogging.',
     author='Martey Dodoo',
     author_email='django-longform@marteydodoo.com',
     url='https://github.com/martey/django-longform',
     license='MIT',
+    packages=['longform'],
     long_description=open('README').read(),
     classifiers=[],
     install_requires=[
