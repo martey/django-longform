3c1f8685b3c2555d26c9630414936894416030ad
Martey Dodoo <martey@marteydodoo.com>
2012-09-20 21:07:23 -0400

    Add setup.py for PyPi.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='django-longform',
+    version='0.1.0',
+    description='A Django application for longform blogging.',
+    author='Martey Dodoo',
+    url='http://github.com/martey/django-longform',
+    license='MIT',
+    py_modules=[
+        'longform',
+    ],
+    long_description=read("README.rst"),
+    classifiers=[],
+)
